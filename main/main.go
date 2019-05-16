529561264c61ea03b6afd06d8d2c7a8cba0ba218
John Dow <JLDJLD503@gmail.com>
2019-05-16 00:40:50 -0500

    traversing a singularly linked lsit

--- main/main.go
+++ main/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/mux"
 	"log"
-	"net/http"
 	"template/types"
 )
 
@@ -13,41 +10,8 @@ type ExampleLister struct {
 }
 
 func main() {
-	exampler := ExampleLister{}
-	r := mux.NewRouter()
-
-	r.HandleFunc("/example", exampler.PostExample).Methods("POST")
-	r.HandleFunc("/examples", exampler.GetExample).Methods("GET")
-
-	err := http.ListenAndServe(":8080", r)
-	log.Print(err)
-}
-
-func (y *ExampleLister) PostExample(w http.ResponseWriter, r *http.Request) {
-	var example types.Example
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&example)
-	if err != nil {
-		log.Print("submitted example could not be decoded")
-		return
-	}
-
-	y.examples = append(y.examples, example)
-
-	jsonResponse(w, http.StatusOK, y.examples)
-}
-
-func(y *ExampleLister) GetExample(w http.ResponseWriter, r *http.Request) {
-	jsonResponse(w, http.StatusOK, y.examples)
-}
-
-func jsonResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	_, err := w.Write(response)
-	if err != nil {
-		log.Print(err)
+	example := types.ExampleLinkedList()
+	for ex := example; ex != nil; ex = ex.NextNode {
+		log.Print(ex.FirstName + " " + ex.LastName)
 	}
 }
--- types/example.go
+++ types/example.go
@@ -5,4 +5,32 @@ package types
 type Example struct {
 	FirstName string
 	LastName  string
+	NextNode  *Example
+}
+
+func ExampleLinkedList() *Example {
+	firstNode := NewExample("john", "snow", nil)
+	AppendNode(NewExample("jennifer", "mccollum", nil), &firstNode)
+	AppendNode(NewExample("billy", "bob", nil), &firstNode)
+	AppendNode(NewExample("courtney", "turner", nil), &firstNode)
+	return &firstNode
+}
+
+func NewExample(first string, last string, nextNode *Example) Example {
+	return Example{
+		FirstName: first,
+		LastName:  last,
+		NextNode:  nextNode,
+	}
+}
+
+func AppendNode(example Example, exampleList *Example) *Example {
+	for currentExample := exampleList; currentExample != nil; currentExample = currentExample.NextNode {
+		if currentExample.NextNode == nil {
+			currentExample.NextNode = &example
+			return exampleList
+		}
+	}
+
+	return exampleList
 }
